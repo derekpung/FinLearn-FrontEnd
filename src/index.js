7385154dea994e1763cbef1d614039918c97fdd0
Tanya <tanya.campos898@gmail.com>
2021-09-17 02:14:37 +0800

    prile 9.16

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './styles/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from '@src/Context'
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <StyledEngineProvider>
       <AppProvider>
         <App />
       </AppProvider>
+      </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import Page from '@components/Page'
+import Page from '@components/Page';
+
+
 
 function Profile() {
-
+  let tokens = 8.26; 
+  // define function for the tokens above
   return (
     <Page pageTitle="Profile">
-      content
+      <h3>James Smith</h3>
+      <div className="sideby">
+        
+      <i class='fas fa-coins' style={{ fontSize: "50px", color:'grey' }} ></i>
+      <h3> LTE Tokens:{tokens}</h3>
+      {/* style={{ fontSize: "80px", color:'grey' }} */}
+      </div>
+      
     </Page>
   )
 }

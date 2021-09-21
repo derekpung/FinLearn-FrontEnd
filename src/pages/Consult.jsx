c067caa0013d12580c90cf38121337c98f8f0462
Derek <derekpung@hotmail.com>
2021-09-21 21:46:45 +0800

    consult and courses

--- src/pages/Consult.jsx
+++ src/pages/Consult.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import Page from '@components/Page'
+import Page, { PageSection } from '@components/Page'
+import Empty from '@components/Empty'
+import { Button } from '@mui/material'
+import { HiOutlineChatAlt2 } from 'react-icons/hi'
+import { useAppContext } from '@src/Context';
+
 
 function Consult() {
+  const { notImplemented } = useAppContext()
 
   return (
-    <Page pageTitle="Consult">
-      content
+    <Page pageTitle="Consultations">
+      <PageSection >
+        {/* List of Consultations | Redirect Consultants */}
+        <Empty
+          title="You do not have any consultations (yet)"
+          body="Consult with top experts around the world with LTE tokens"
+          icon={<HiOutlineChatAlt2/>}
+          button={
+            <Button 
+              onClick={notImplemented}
+              variant="contained">Explore Consultants</Button>
+          }/>
+      </PageSection>
     </Page>
   )
 }
--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import Page from '@components/Page';;
+import Page, { PageSection } from '@components/Page'
+import Empty from '@components/Empty'
+import { Button } from '@mui/material'
+import { HiOutlineBookOpen } from 'react-icons/hi'
+import { useAppContext } from '@src/Context';
+
 
 function Progress() {
-
+  const { notImplemented } = useAppContext()
+  // api call for list of courses
   return (
     <Page pageTitle="My Courses">
-      content
+      <PageSection >
+        {/* List of taken courses | Redirect Course list */}
+        <Empty
+          title="You have not enrolled in any courses (yet)"
+          body="Start by enrolling in a course and learn to earn LTE tokens"
+          icon={<HiOutlineBookOpen/>}
+          button={
+            <Button 
+              onClick={notImplemented}
+              variant="contained">Explore Courses</Button>
+          }/>
+      </PageSection>
     </Page>
   )
 }

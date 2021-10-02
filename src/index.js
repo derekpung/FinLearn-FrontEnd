f243864540916af267f6d4da0a149b64c025f7d3
Derek <derekpung@hotmail.com>
2021-10-02 22:46:53 +0800

    git actions init

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { AppProvider } from '@src/Context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppProvider>
         <App />
       </AppProvider>
